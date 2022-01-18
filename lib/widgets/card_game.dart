a1945ba1602ba4e2be2a0af410e5f789ca64b581
Wallace Holanda <wallace.holanda@hotmail.com>
2022-01-18 20:34:49 -0300

    Adicionando a animacao ao clicar nos cards

--- lib/widgets/card_game.dart
+++ lib/widgets/card_game.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -18,33 +19,77 @@ class CardGame extends StatefulWidget {
   _CardGameState createState() => _CardGameState();
 }
 
-class _CardGameState extends State<CardGame> {
+class _CardGameState extends State<CardGame>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animation = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+  }
+
+  @override
+  void dispose() {
+    animation.dispose();
+    super.dispose();
+  }
+
+  flipCard() {
+    if (!animation.isAnimating) {
+      animation.forward(); //Faz com que a carta 'vire'
+      Timer(const Duration(seconds: 2), () => animation.reverse());
+    }
+  }
+
+  // Caso a carta tenha atingido 90 graus, a imagem aparecerá
+  AssetImage getImage(double angulo) {
+    if (angulo > 0.5 * pi || angulo == 0.2 * pi) {
+      return const AssetImage(
+          'images/atreus.png'); // Substituir por ${widget.opcao.toString()}
+    } else {
+      return widget.modo == Modo.mortal
+          ? const AssetImage('images/cardMortal.png')
+          : const AssetImage('images/cardGow.png');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final angulo = 0.0 * pi;
-    final transform = Matrix4.identity()
-      ..setEntry(3, 2, 0.002)
-      ..rotateY(angulo);
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext context, _) {
+        final angulo = animation.value * pi;
+        final transform = Matrix4.identity()
+          ..setEntry(3, 2, 0.002)
+          ..rotateY(angulo);
 
-    return Transform(
-      transform: transform,
-      alignment: Alignment.center,
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color:
-                widget.modo == Modo.mortal ? Colors.white : GodOfWarTheme.color,
-            width: 2,
+        return GestureDetector(
+          onTap: () => flipCard(),
+          child: Transform(
+            transform: transform,
+            alignment: Alignment.center,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: widget.modo == Modo.mortal
+                      ? Colors.white
+                      : GodOfWarTheme.color,
+                  width: 2,
+                ),
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: getImage(angulo),
+                ),
+              ),
+            ),
           ),
-          borderRadius: const BorderRadius.all(Radius.circular(5)),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: widget.modo == Modo.mortal
-                ? const AssetImage('images/cardMortal.png')
-                : const AssetImage('images/cardGow.png'),
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
